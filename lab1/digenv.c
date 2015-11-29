6688af185b09732ff1b2c1121e99bf9349ff6bf8
Oscar Melin <oscarme@kth.se>
2015-11-29 18:32:51 +0100

    add dup fd and add closing of pfd

--- lab1/digenv.c
+++ lab1/digenv.c
@@ -12,13 +12,33 @@ void pipes_init() {
     int ret;
     int i;
 
-    for(i = 0; i < num_procs - 1; ++i) {
+    for (i = 0; i < num_procs - 1; ++i) {
 
         ret = pipe(pipes[i]);
 
-        if(ret == -1) {
+        if (ret == -1) {
+
             perror("pipe");
             exit(1);
+        }
+    }
+}
+
+void close_all_fd() {
+
+    int i;
+    int j;
+
+    for (i = 0; i < num_pipes; i++) {
+        
+        for (j = 0; j < 2; j++) {
+            
+            int ret = close(pipes[i][j]);
+            if (ret == -1) {
+                
+                perror("close");
+                exit(-1);
+            }
         }
     }
 }
@@ -28,12 +48,30 @@ int exec_proc(int fd_in, int fd_out, char *program, char *argv[]) {
     int pid = fork();
 
     if (pid == 0) {
+        
+        /*Duplicate stdin and stdout file descriptors*/
+        int ret;
 
+        ret = dup2(fd_in, STDIN_FILENO);
+        if (ret == -1) {
 
+            perror("exec_proc");
+            exit(-1);
+        }
+        ret = dup2(fd_out, STDOUT_FILENO);
+        if (ret == -1) {
+            
+            perror("exec_proc");
+            exit(-1);
+        }
 
+        /*Close all file descriptors in pipes*/
+        close_all_fd();        
+
+                
 
     } else if (pid == -1) {
-        perror("exec_proc()");
+        perror("exec_proc");
         exit(-1);
     }
 
@@ -43,7 +81,7 @@ int main(int argc, char *argv[]) {
 
     char *pager = getenv("PAGER");
 
-    if(pager == NULL)
+    if (pager == NULL)
         pager = "less";
 
     args = (argc > 1) ? 1 : 0;
@@ -52,7 +90,6 @@ int main(int argc, char *argv[]) {
 
     /*Initialize pipes*/
     pipes_init();
-
     /* Set pager */
 
     /* Execute printenv */
@@ -66,7 +103,7 @@ int main(int argc, char *argv[]) {
     /* Close all file descriptors*/
 
     /* Wait for all processes to finish */
-
+/*
     if( argc == 2 ) {
 
         printf("The argument supplied is %s\n", argv[1]);
@@ -79,5 +116,6 @@ int main(int argc, char *argv[]) {
 
         printf("One argument expected.\n");
     }
+*/
     return 0;
 }
