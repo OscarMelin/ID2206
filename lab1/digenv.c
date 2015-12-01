a70b6ccc250ec5753b34722b3901c40a22194370
Oscar Melin <oscarme@kth.se>
2015-12-01 18:29:13 +0100

    Add default to more if no less Add comments

--- lab1/digenv.c
+++ lab1/digenv.c
@@ -1,15 +1,28 @@
 /*
-TODO
+NAME:
 
-SET PAGER!?
+    digenv - view enviroment variablels in a sorted list w/ or w/o regex filter
 
-CLEAN UP!?
+SYNTAX:
+    
+    digenv [REGEX]
 
-SIGNAL HANDELER!?
+DESCRIPTION:
+
+    digenv display the enviroment variables in sorted order using the default
+    pager specified in the enviroment variables.
+    If a regex pattern is provided, digenv will display a filtered version of
+    the output described above.
+
+OPTIONS:
+
+    See grep(1)
+
+EXAMPLES:
+
+    digenv
+    digenv "PATTERN" 
 */
-
-
-
 
 
 #include <stdio.h>
@@ -23,7 +36,10 @@ int args;
 int num_procs;
 int num_pipes;
 int pipes[3][2];
+char *pager;
 
+
+/* Initializes all required pipes */
 void pipes_init() {
 
     int ret;
@@ -41,6 +57,7 @@ void pipes_init() {
     }
 }
 
+/* Closes all pipe file descriptors */
 void close_all_fd() {
 
     int i;
@@ -60,6 +77,7 @@ void close_all_fd() {
     }
 }
 
+/* Waits for att child processes to finish */
 void wait_for_kids() {
 
     int status;
@@ -73,21 +91,26 @@ void wait_for_kids() {
             
             /* Process exited abnormaly */
             perror("wait_for_kids");
-            /*TODO Some cleanup funtion call??*/
             kill(0, SIGTERM);
             exit(-1);        
         }
     }
 }
 
-int exec_proc(int fd_in, int fd_out, char *program, char *argv[]) {
+/*
+Forks and executes the "program" with arguments args in a different process.
+STDIN and STDOUT are redirected to fd_in and fd_out and used for the required
+inter-process communication. 
+*/
+pid_t exec_proc(int fd_in, int fd_out, char *program, char *argv[]) {
     
-    int pid = fork();
+    pid_t pid = fork();
 
     if (pid == 0) {
         
         /*Duplicate stdin and stdout file descriptors*/
         int ret;
+        char *ptr[2];
 
         ret = dup2(fd_in, STDIN_FILENO);
         if (ret == -1) { 
@@ -102,19 +125,33 @@ int exec_proc(int fd_in, int fd_out, char *program, char *argv[]) {
             exit(-1);
         }
 
-        /*Close all file descriptors in pipes*/
+        /* Close all file descriptors in pipes */
         close_all_fd();        
         
-        /*check if grep*/
+        /* NULL args for all programs but grep */
+        ptr[0] = program;
+        ptr[1] = (char*) NULL;
+        
+        /* Execute */
         if (!strcmp(program, "grep")) {
             
             execvp(program, argv);
-            exit(-1);        
+            exit(-1);
+
+        } else if (!strcmp(program, pager)) {
+
+            int exec_ret;
+
+            exec_ret = execvp(program, ptr);
+
+            /* If pager fails, default to more */
+            if (exec_ret == -1) {
+                
+                execvp("more", ptr);
+            }
+
         } else {
 
-            char *ptr[2];
-            ptr[0] = program;
-            ptr[1] = (char*) NULL;
             execvp(program, ptr);
             exit(-1);
         }
@@ -124,42 +161,36 @@ int exec_proc(int fd_in, int fd_out, char *program, char *argv[]) {
         perror("exec_proc");
         exit(-1);
     }
-    return 0;
+    return pid;
 }
 
 int main(int argc, char *argv[]) {
 
-    char *pager = getenv("PAGER");
+    pager = getenv("PAGER");
 
     if (pager == NULL)
         pager = "less";
 
+    /* Set number of processes and pipes depending on if we should run grep */
     args = (argc > 1) ? 1 : 0;
     num_procs = 3 + args;
     num_pipes = 2 + args;
 
-    /*Initialize pipes*/
     pipes_init();
-    /* Set pager */
 
-    /* Execute printenv */
     exec_proc(STDIN_FILENO, pipes[0][1], "printenv", argv); 
 
-    /* Check and execute grep */
     if (args) {
 
         exec_proc(pipes[0][0], pipes[args][1], "grep", argv);
     }
-
-    /* Execute sort */
     exec_proc(pipes[args][0], pipes[1 + args][1], "sort", argv);
 
-    /* Execute less or more */
     exec_proc(pipes[1 +args][0], STDOUT_FILENO, "less", argv);
 
-    /* Close all file descriptors*/
     close_all_fd();
-    /* Wait for all child processes to terminate */
+
     wait_for_kids();
+
     return 0;
 }
