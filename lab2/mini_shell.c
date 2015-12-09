e3c9d35a9cb55199c7927d9df3cfe19e7c8f5d57
Oscar Melin <oscarme@kth.se>
2015-12-10 00:38:50 +0100

    asdasdas

--- lab2/mini_shell.c
+++ lab2/mini_shell.c
@@ -20,18 +20,27 @@ EXAMPLES:
 
 int exit_mini_shell = 0; /*1 if exit*/
 
-pid_t exec_proc(int fd_in, int fd_out, char *program, char *params[]) {
+pid_t exec_proc(char *program, char *params[], int background) {
     
     pid_t pid = fork();
+    int pipes[2];
+    int status;
+    
+    if (background) {
+
+        pipe(pipes);    
+    }
 
     pid = fork();
+
     if ( pid == 0 ) { /* child-process kod */
 
-
+        execvp(program, params);
+        exit(-1);
     }
     else if ( pid > 0) { /* parent-process kod */
 
-
+        waitpid(pid, &status, 0);
     }
     else { /* SYSTEM ERROR */
 
@@ -39,6 +48,13 @@ pid_t exec_proc(int fd_in, int fd_out, char *program, char *params[]) {
         exit(-1);
     }
 
+    if (background) {
+    
+    /* Close pipes */
+    } else {
+
+
+    }
     return pid;
 }
 
@@ -78,7 +94,8 @@ int exec_program(char *str) {
 
     /* TODO: Check if & as last param, then run as background */
 
-
+    /* Foreground */
+        exec_proc(params[0], params, 0);
     }
 
     return 0;
