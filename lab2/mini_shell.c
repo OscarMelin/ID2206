ad4914a477f003641a124e3c7610c1de2fe11067
Oscar Melin <oscarme@kth.se>
2015-12-09 17:35:13 +0100

    add lab2 mini_shell.c

---
+++ lab2/mini_shell.c
@@ -0,0 +1,26 @@
+/*
+NAME:
+
+SYNTAX:
+
+DESCRIPTION:
+
+OPTIONS:
+
+EXAMPLES:
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
