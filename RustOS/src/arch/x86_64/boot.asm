c934c1ce4c3bf7d8856347f7c89ff4b71060232f
Oscar Melin <oscarme@kth.se>
2016-11-13 01:20:36 +0100

    asdf

--- RustOS/src/arch/x86_64/boot.asm
+++ RustOS/src/arch/x86_64/boot.asm
@@ -5,6 +5,10 @@ bits 32
 start:
     mov esp, stack_top
 
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
