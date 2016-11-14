534bbc437736186758904807e752ce6b578ff233
Oscar Melin <oscarme@kth.se>
2016-11-14 23:12:15 +0100

    catchups ..

--- Experiments/Processes/fork.c
+++ Experiments/Processes/fork.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
 
 	int pid = fork();
 
-	printf("pid = %d\n", pid);
+	if (pid == 0) {
+		printf("im child %d\n", getpid());
+		sleep(1);
+	} else {
+		printf("My child is called %d\n", pid);
+		wait(NULL);
+	}
+	printf("End (%d)\n", getpid());
 
 	return 0;
 }
